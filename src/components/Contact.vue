1d5455384bcc44dacdd76a8998d95d01f4c4c2cc
Vivian Hayes <vivian.xw7@gmail.com>
2023-12-09 16:35:49 -0600

    add components

---
+++ src/components/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h1>Contact Me</h1>
+      <p>Feel free to reach out to me through the following channels:</p>
+      <ul>
+        <li>Email: [Your Email]</li>
+        <li>LinkedIn: [Your LinkedIn]</li>
+        <li>GitHub: [Your GitHub]</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Contact',
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
+  
---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Welcome to My Portfolio</h1>
+        <p>Hi, I'm [Your Name]. I'm a web developer passionate about creating awesome web experiences.</p>
+    </div>
+</template>
+
+<script>
+export default {
+name: 'Home',
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
+  
