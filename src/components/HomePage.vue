993901b13b8c50931a5290fc52f82120ebca035e
Vivian Hayes <vivian.xw7@gmail.com>
2023-12-19 22:27:49 -0600

    make content dynamic

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,13 +1,41 @@
 <template>
     <div>
-        <h1>Welcome to My Portfolio</h1>
-        <p>Hi, I'm [Your Name]. I'm a web developer passionate about creating awesome web experiences.</p>
+        <!-- <h1>Welcome to My Portfolio</h1>
+        <p>Hi, I'm Vivian. I'm a web developer passionate about creating awesome web experiences.</p> -->
+
+        <p>{{ introduction }}</p>
     </div>
 </template>
 
 <script>
 export default {
-name: 'HomePage',
+  name: 'HomePage',
+  data() {
+    return {
+      introduction: '', // Initialize with an empty string
+    };
+  },
+  mounted() {
+    // Fetch data from WordPress API or other source
+    this.fetchIntroduction();
+  },
+  methods: {
+    async fetchIntroduction() {
+      try {
+        // Replace this URL with your WordPress API endpoint
+        const response = await fetch('https://your-wordpress-site/wp-json/your-custom-endpoint');
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+
+        const data = await response.json();
+        this.introduction = data.introduction; // Adjust the property name based on your API response
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
+  },
 };
 </script>
 
--- src/components/MyProjects.vue
+++ src/components/MyProjects.vue
@@ -11,7 +11,7 @@
   
   <script>
   export default {
-    name: 'Projects',
+    name: 'MyProjects',
     data() {
       return {
         projects: [
