843ddbcb05a80c4a0a421a575ecda5c106e3f982
Vivian Hayes <vivian.xw7@gmail.com>
2023-12-19 22:15:15 -0600

    add home component

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,21 @@
 
     <AppHeader />
 
+    <HomePage />
+
     <AppFooter />
   </div>
 </template>
 
 <script>
+import HomePage from './components/HomePage.vue';
 import AppHeader from './components/AppHeader.vue';
 import AppFooter from './components/AppFooter.vue';
 
 export default {
   name: 'App',
   components: {
+    HomePage,
     AppHeader,
     AppFooter,
   },
