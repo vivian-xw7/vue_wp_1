2b235f0e4c04f42d3469ae72c2b4bb6f8c9c2eab
Vivian Hayes <vivian.xw7@gmail.com>
2023-12-25 17:59:39 -0600

    extract projects view

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,14 +1,37 @@
 <template>
   <div>
-    <h1>My Projects</h1>
-    <!-- Add content related to projects -->
-  </div>
+      <h1>My Projects</h1>
+      <div v-for="project in projects" :key="project.id">
+        <h2>{{ project.title }}</h2>
+        <p>{{ project.description }}</p>
+        <img :src="project.image" alt="Project Image" />
+      </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'ProjectsView',
-};
+    name: 'ProjectsView',
+    data() {
+      return {
+        projects: [
+          {
+            id: 1,
+            title: 'Project 1',
+            description: 'Description of Project 1.',
+            image: 'project1.jpg',
+          },
+          {
+            id: 2,
+            title: 'Project 2',
+            description: 'Description of Project 2.',
+            image: 'project2.jpg',
+          },
+          // Add more projects as needed
+        ],
+      };
+    },
+  };
 </script>
 
 <style scoped>
