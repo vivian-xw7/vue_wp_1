7c9d84b8b668de9bdb6dbcb274e531d6a14562be
Vivian Hayes <vivian.xw7@gmail.com>
2023-12-25 17:57:25 -0600

    extract home view

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,17 @@
   <div id="app">
     <AppHeader />
 
-    <HomePage />
-
     <AppFooter />
   </div>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue';
 import AppHeader from './components/AppHeader.vue';
 import AppFooter from './components/AppFooter.vue';
 
 export default {
   name: 'App',
   components: {
-    HomePage,
     AppHeader,
     AppFooter,
   },
@@ -27,4 +23,3 @@ export default {
 /* Your styles for the App component */
 </style>
 
-<!-- C:\Users\Vivian\Local Sites\vue-wp\app\public\wp-content\themes\vue_wp_1\src\components\AppHeader.vue -->
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,48 @@
 <template>
     <div>
       <h1>Welcome to My Portfolio</h1>
+      <h2>THIS IS THE VIEW</h2>
       <p>Hi, I'm Vivian. I'm a web developer passionate about creating awesome web experiences.</p>
     </div>
+
+    <!-- <div>
+      <p>{{ introduction }}</p>
+    </div> -->
   </template>
   
   <script>
   export default {
     name: 'HomeView',
+    data() {
+      return {
+        introduction: '',
+      };
+    },
+    mounted() {
+      // Fetch data from WordPress API
+      this.fetchIntroduction();
+    },
+    methods: {
+      async fetchIntroduction() {
+        try {
+          const response = await fetch('http://vue-wp.local/wp-json/'); // fetches the API of the site
+  
+          if (!response.ok) {
+            throw new Error('Failed to fetch data');
+          }
+  
+          const data = await response.json();
+        //   console.log('API Response:', data)
+
+          this.introduction = data.description;
+
+
+        //   this.introduction = data.content.rendered; // Adjust the property name based on your API response
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      },
+    },
   };
   </script>
   
