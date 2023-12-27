e1c98ac91da1b17fb7b474630dae64a04d406df8
Vivian Hayes <vivian.xw7@gmail.com>
2023-12-26 20:58:12 -0600

    header styles

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -3,9 +3,13 @@
       <h1>Contact Me</h1>
       <p>Feel free to reach out to me through the following channels:</p>
       <ul>
-        <li>Email: [Your Email]</li>
-        <li>LinkedIn: [Your LinkedIn]</li>
-        <li>GitHub: [Your GitHub]</li>
+        <li>Email: vivian.xw7@gmail.com</li>
+        <li>
+          <a href="https://github.com/vivian-xw7" class="">Github</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/vivian-hayes-4629911a2/" class="">LinkedIn</a>
+        </li>
       </ul>
     </div>
   </template>
