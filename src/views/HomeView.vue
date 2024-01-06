7d4f9c56d87a3018689782768eb0b2ac7e105183
Vivian Hayes <vivian.xw7@gmail.com>
2024-01-06 16:47:00 -0600

    getting title of post

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,11 +17,12 @@
 
     <div class="container">
       <p v-if="isLoading">Loading...</p>
-      <ul v-for="p in post" :key="p.id">
+      <div>{{ post.length > 0 ? post[0].title.rendered : '' }}</div>
+      <!-- <ul v-for="p in post" :key="p.id">
         <h1>{{ p.title }}</h1>
         <p>{{ p.content }}</p>
         <p class="date">{{ p.createdAt }}</p>
-      </ul>
+      </ul> -->
     </div>
 
     <!-- <div>
